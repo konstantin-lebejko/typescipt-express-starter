69dda8f923b1d9a82a72ddf2313d6547ac38e1df
Konstantin Lebejko <konstantin.lebejko@gmail.com>
2022-10-12 19:35:52 +0200

    added eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = process.env.PORT || 5001;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("typescript server run...");
+app.get('/', (req: Request, res: Response) => {
+  console.log('zap');
+  res.send('typescript server run...');
 });
 
 app.listen(port, () => console.log(`app listen on ${port}`));
